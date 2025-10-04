8d50a0edd785f52a67d5f1104aee721460f1f1d1
Avinash kumar <akum97543@gmail.com>
2025-10-05 01:48:09 +0530

    docs(swagger): added and configured Swagger UI for API documentation

--- src/main/java/com/avinash/urlshortener/config/OpenAPIConfig.java
+++ src/main/java/com/avinash/urlshortener/config/OpenAPIConfig.java
@@ -1,26 +1,26 @@
 package com.avinash.urlshortener.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class OpenApiConfig {
+public class OpenAPIConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("URL Shortener API")
+                        .title("URL Shortener API 🚀")
                         .version("1.0.0")
-                        .description("📘 API documentation for the Spring Boot + MySQL + Redis based URL Shortener App")
+                        .description("API documentation for the URL Shortener project built using Java & Spring Boot.")
                         .contact(new Contact()
                                 .name("Avinash Kumar")
                                 .email("avinash@example.com")
-                                .url("https://www.linkedin.com/in/avinash-java-backend"))
+                                .url("https://github.com/Avinashkr000"))
                         .license(new License()
                                 .name("Apache 2.0")
                                 .url("http://springdoc.org")));
